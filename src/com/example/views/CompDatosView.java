4b2402bca40e143cd5f9451002730746a436d0bb
Nicles <nicohuvi@gmail.com>
2014-09-29 21:51:44 -0300

    Se agrega la persistencia a la vista en la que se completan los datos adicionales

--- src/com/example/views/CompDatosView.java
+++ src/com/example/views/CompDatosView.java
@@ -2,24 +2,34 @@ package com.example.views;
 
 import java.text.NumberFormat;
 import java.util.Locale;
+
 import com.vaadin.data.util.converter.StringToLongConverter;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.example.domain.Career;
+import com.example.domain.PersonalInfo;
+import com.example.domain.User;
+import com.example.persistence.CareerManager;
+import com.example.persistence.UserManager;
 import com.example.vaadintest01.Vaadintest01UI;
 import com.example.views.templates.AbstractHomeView;
+import com.vaadin.server.VaadinSession;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.DateField;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 
 public class CompDatosView extends AbstractHomeView implements View {
-
 	
+	UserManager manUsuario = new UserManager();
+	User usuario = new User();
+	PersonalInfo persInfo = new PersonalInfo();
 	private static final long serialVersionUID = 1L;
 
 	public CompDatosView(){}
@@ -27,43 +37,27 @@ public class CompDatosView extends AbstractHomeView implements View {
 	public void enter(ViewChangeEvent event){
 		super.enter(event);
 		
+		
 		//Primero vamos a crear un form que es donde el usuario va a cargar los datos y luego se va a crear el panel
 		//en donde se va a colocar el form
 		FormLayout form = new FormLayout();
-		form.addStyleName("Menu");
-		form.setSizeFull();
+		form.addStyleName("light");
+		form.setWidth("500");
 		form.setSpacing(true);
 		
 		getRightLayout().addComponent(form);
 		getRightLayout().setExpandRatio(form, 1.0f);
 
 		
-		Button aceptar = new Button("Aceptar",
-				new Button.ClickListener() {
-				private static final long serialVersionUID = 1L;
-
-		//@Override
-		public void buttonClick(ClickEvent event) {
-			getUI().getNavigator().navigateTo(Vaadintest01UI.HOME_VIEW);
-		}
-	}); //Aca se crea el boton aceptar que te envia a la Home
-		Button cancelar = new Button("Cancelar",
-				new Button.ClickListener() {
-				private static final long serialVersionUID = 1L;
-
-		//@Override
-		public void buttonClick(ClickEvent event) {
-			getUI().getNavigator().navigateTo(Vaadintest01UI.MAIN_VIEW);
-		}
-	}); //Lo mismo que el anterior pero para volver al Main
-		DateField fecha = new DateField("Fecha de nacimiento");
-		
-		ComboBox carr = new ComboBox("Carrera");
-		carr.addItems("Ingenieria en sistemas","Ingenieria química","Ingenieria eléctrica","Ingenieria mecánica");
+				final DateField fecha = new DateField("Fecha de nacimiento");
+		final CareerManager manager = new CareerManager();
+		final ComboBox carr = new ComboBox("Carrera", manager.getContainer());
+		carr.setItemCaptionPropertyId("name");
+		//carr.addItems("Ingenieria en sistemas","Ingenieria química","Ingenieria eléctrica","Ingenieria mecánica");
 		//El comboBox es el menu desplegable para elegir la carrera. En la documentacion esta la forma de como manejar
 		//las seleccion para cuando necesitemos guardarlo en la base de datos
-		ComboBox taño = new ComboBox("Año");
-		taño.addItems("1","2","3","4","5");
+		final ComboBox taño = new ComboBox("Año");
+		taño.addItems(1,2,3,4,5);
 		carr.setNullSelectionAllowed(false);
 		taño.setNullSelectionAllowed(false);
 		carr.setTextInputAllowed(false);
@@ -74,10 +68,11 @@ public class CompDatosView extends AbstractHomeView implements View {
 		taño.setRequiredError("Obligatorio");
 		
 		
+		
 	
 		
 		//Se crea el textfield para teléfono asegurándose que lo ingresado sea un numero y no un string
-		TextField tel = new TextField("Teléfono");
+		final TextField tel = new TextField("Teléfono");
 		tel.setImmediate(true);
 	
 		StringToLongConverter convertidorPlano = new StringToLongConverter() {
@@ -99,12 +94,56 @@ public class CompDatosView extends AbstractHomeView implements View {
 		
 		
 		
-		TextField ciu = new TextField("Ciudad");
+		final TextField ciu = new TextField("Ciudad");
 		form.addComponent(carr);
 		form.addComponent(taño);
 		form.addComponent(ciu);
 		form.addComponent(tel);
 		form.addComponent(fecha);
+		
+		
+		Button aceptar = new Button("Aceptar",
+				new Button.ClickListener() {
+				private static final long serialVersionUID = 1L;
+
+		//@Override
+		public void buttonClick(ClickEvent event) {
+			if (carr.isValid() & taño.isValid()) {
+				persInfo.setCity(ciu.getValue());
+				persInfo.setPhone((Long)tel.getConvertedValue());
+				usuario.setPersonalinfo(persInfo);
+				usuario.setCareer((Career)carr.getData()); 
+				usuario.setYear((Integer)taño.getValue()); 
+				usuario.setBirthday(fecha.getValue()); 
+				usuario.setEmail("nicohuvi@gmail.com");
+				manUsuario.save(usuario);
+				getUI().getNavigator().navigateTo(Vaadintest01UI.HOME_VIEW);
+				
+				
+				
+			}
+			else {
+				
+				Notification.show("Revise los campos obligatorios\n",
+						"Los campos obligatorios son la carrera y el año que está cursando",
+		                Notification.Type.WARNING_MESSAGE);
+				
+			}
+			
+		}
+	}); //Aca se crea el boton aceptar que te envia a la Home
+		Button cancelar = new Button("Cancelar",
+				new Button.ClickListener() {
+				private static final long serialVersionUID = 1L;
+
+		//@Override
+		public void buttonClick(ClickEvent event) {
+
+			getUI().getNavigator().navigateTo(Vaadintest01UI.MAIN_VIEW);
+		}
+	}); //Lo mismo que el anterior pero para volver al Main
+
+		
 		
 		HorizontalLayout botones= new HorizontalLayout();
 		botones.addComponent(aceptar);
@@ -117,9 +156,14 @@ public class CompDatosView extends AbstractHomeView implements View {
 		panel.setContent(form);
 		getRightLayout().addComponent(panel);
 		getRightLayout().setExpandRatio(panel, 1.0f);
+		
+		
+		
 
 	}
 	
+
+
 	class ButtonListener implements Button.ClickListener {
 
 		private static final long serialVersionUID = 1L;
@@ -141,4 +185,3 @@ public class CompDatosView extends AbstractHomeView implements View {
 	
 
 }
-
